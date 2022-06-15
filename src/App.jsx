3bb1c4bb5657c72c6aeac67bb5183b4052c67b3f
ducnam0902 <ducnam0902@gmail.com>
2022-06-15 15:21:39 +0700

    'chore:create-footer'

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.scss";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<a>sa</a>} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
