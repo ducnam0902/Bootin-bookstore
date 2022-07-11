303b09dc8b9e5c4bfa94db0b51dcd960a702c3d9
ducnam0902 <ducnam0902@gmail.com>
2022-07-11 14:13:05 +0700

    chore: update stub data

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import MockApdater from "axios-mock-adapter";
 let mock = new MockApdater(axios);
 
-import popularBook from "@bootin/stub/popularBook.json";
+import { popularBook } from "@bootin/stub/popularBook.js";
 export const fetchPopularBook = async () => {
   let body;
   mock.onGet("/popularBook").reply(200, popularBook);
---
+++ src/stub/popularBook.js
@@ -0,0 +1,53 @@
+import {
+  popularBook1,
+  popularBook2,
+  popularBook3,
+  popularBook4,
+  popularBook5,
+  popularBook6,
+} from "@bootin/assets/image";
+
+export const popularBook = [
+  {
+    image: popularBook1,
+    bookTitle: "Castle In The Sky",
+    author: "Susan Merrlin",
+    price: "70.00",
+    favouriteStar: 3,
+  },
+  {
+    image: popularBook2,
+    bookTitle: "Art Fashion",
+    author: "Sabela Hupter",
+    price: "205.00",
+    favouriteStar: 4,
+  },
+  {
+    image: popularBook3,
+    bookTitle: "Cup Cake Diraes",
+    author: "Anna Hillton",
+    price: "20.5",
+    favouriteStar: 3,
+  },
+  {
+    image: popularBook4,
+    bookTitle: "Inspiration Speaks",
+    author: "Anna Hillton",
+    price: "30.51",
+    favouriteStar: 5,
+  },
+  {
+    image: popularBook5,
+    bookTitle: "A Thousand Seed",
+    author: "Sabela Hupter",
+    price: "20.30",
+    favouriteStar: 2,
+  },
+  {
+    image: popularBook6,
+    bookTitle: "A God Who Hates Women",
+    author: "Susan Merrlin",
+    price: "16.51",
+    favouriteStar: 5,
+  },
+];
